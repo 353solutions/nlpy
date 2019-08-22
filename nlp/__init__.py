d4c34377bc2b84dcfc37f021e833785777c4677c
Miki Tebeka <miki.tebeka@gmail.com>
2019-08-22 17:21:07 +0300

    docstring

--- nlp/__init__.py
+++ nlp/__init__.py
@@ -1,3 +1,12 @@
+"""NLP package for Python
+
+>>> import nlp
+>>> nlp.tokenize('Walking on sunshine!')
+['walk', 'sunshine']
+>>> [nlp.stem(word) for word in ['works', 'working', 'work']]
+['work', 'work', 'work']
+"""
+
 import re
 
 from .stop_words import stop_words
